82ad4f96935b938830780f46eecacdf8f94adb78
jeyantharan <87352558+jeyantharan@users.noreply.github.com>
2022-03-11 16:06:37 +0530

    1st commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// const mongoConnectionString = process.env.DB_URI;
+const mongoConnectionString ="mongodb://localhost:27017/MyServiceCube";
+//should change the database name
+const cors = require("cors");
+
+const app = express();
+
+// cors
+app.use(cors());
+app.use(express.json());
+
+//mongoose connecting
+mongoose.connect(mongoConnectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+const con = mongoose.connection;
+
+con.on("open", () => {
+  console.log("Database connected");
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const userRoutes = require("./AuthService/routes/userRoutes");
+const resetPassword = require("./AuthService/routes/resetRoutes");
+const forgetPasswordRoutes = require("./AuthService/routes/forgetPasswordRoutes");
+const checkIdentity = require("./AuthService/routes/checkIdentity");
+
+
+app.use("/auth", userRoutes);
+app.use("/account", resetPassword);
+app.use("/forget-password", forgetPasswordRoutes);
+app.use("/check-identity", checkIdentity);
+
+
+app.listen(6000, () => {
+  console.log("Server started at 6000");
+});
