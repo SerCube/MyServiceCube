be2ba1c399d7a0cda319c1fc9df5d45f3ab30da9
jeyantharan <87352558+jeyantharan@users.noreply.github.com>
2022-03-11 16:46:32 +0530

    v

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
 //mongoose connecting
 mongoose.connect(mongoConnectionString, {
   useNewUrlParser: true,
